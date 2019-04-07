59f366d40af7f274ba60696c5df75c5f882ebdb5
aman.pasi@gmail.com <aman.pasi@gmail.com>
2019-04-07 23:30:55 +0530

    formBuilder

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,6 @@
+import { ContactFormComponent } from './../contact-form/contact-form.component';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'new-course-form',
@@ -8,25 +9,33 @@ import { FormGroup, FormArray, FormControl } from '@angular/forms';
 })
 export class NewCourseFormComponent implements OnInit {
 
-  form = new FormGroup({
-    topics: new FormArray([])
-  });
+  form;
 
-  addTopic(topic: HTMLInputElement){
-    this.topics.push(new FormControl(topic.value));
-    topic.value='';
-  }
+  // addTopic(topic: HTMLInputElement){
+  //   this.topics.push(new FormControl(topic.value));
+  //   topic.value='';
+  // }
 
-  removeTopic(topic:FormControl){
-    let index = this.topics.controls.indexOf(topic);
-    this.topics.removeAt(index);
-  }
+  // removeTopic(topic:FormControl){
+  //   let index = this.topics.controls.indexOf(topic);
+  //   this.topics.removeAt(index);
+  // }
 
-  get topics(){
-    return this.form.get('topics') as FormArray;
-  }
+  // get topics(){
+  //   return this.form.get('topics') as FormArray;
+  // }
 
-  constructor() { }
+  constructor(fb: FormBuilder) { 
+    this.form = fb.group({
+      name: ['',Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    });
+
+   }
 
   ngOnInit() {
   }
